b820c5938873017ea8572196ad3f680c0debf264
jnnhyatt <jnnhyatt@gmail.com>
2024-11-06 16:22:45 -0500

    Added backup/restore

--- CanvasPetUnity/Assets/Notification.cs
+++ CanvasPetUnity/Assets/Notification.cs
@@ -19,12 +19,10 @@ public class Notification
         announced = false;
         text = "This should not happen. An error has occured and notification data was lost.";
     }
-
     public string Announce()
     {
         Debug.Assert(announced, "The incoming message has already been announced!");
         announced = true;
-
         return text;
     }
     public void AssignDeath()
@@ -33,14 +31,12 @@ public class Notification
         if (death == null)
         {
             death = now.AddMinutes(ttl);
-        }
-        
+        } 
     }
     public DateTime CheckDeath()
     {
         return death;
     }
-    
     //how long notifications will stay
     public void SetTimeToLive(int mins)
     {
@@ -50,39 +46,28 @@ public class Notification
 public class AutoFeedNotification : Notification
 {
     private int food;
-    private int stored;
-    
-
-
+    private int stored;   
     public AutoFeedNotification(Pet pet, int ate, string asName)
     {
-        
         announced = false;
         petName = pet.name;
         food = ate;
         assignmentName = asName;
-
         text = petName + " ate " + food.ToString() + " food from " + assignmentName + "!\n";
     }
-
     public AutoFeedNotification(Pet pet, int ate, int store, string asName)
-    {
-        
+    {   
         announced = false;
         petName = pet.name;
         food = ate;
         stored = store;
         assignmentName = asName;
-
         text = petName + " ate " + food.ToString() + " food and stored " + stored.ToString() + "from " + assignmentName + "! \n";
     }
-
 }
 public class StorageNotification : Notification
 {
     private int added;
-    
-
     public StorageNotification(Pet pet, int store, string asName)
     {
         announced = false;
@@ -93,11 +78,9 @@ public class StorageNotification : Notification
         text = "Stored " + added.ToString() + " food from " + assignmentName + " for " + petName + "\n";
     }
 }
-
 public class DeadlineNotification : Notification
 {
     private DateTime dueTime;
-
     public DeadlineNotification(string asName,  DateTime dueDate)
     {
         assignmentName = asName;
@@ -105,5 +88,18 @@ public class DeadlineNotification : Notification
         TimeSpan temp = dueTime - DateTime.Now;
         text = assignmentName + " is due in " + temp.TotalHours.ToString() + " hours.\n";
         death = dueTime;
+    }
+}
+public class CoinNotification : Notification
+{
+    private int coinNum;
+    private double grade;
+    public CoinNotification(Pet pet, int num, string asName, double gradeNum)
+    {
+        coinNum = num;
+        assignmentName = asName;
+        grade = gradeNum;
+        coinNum = num;
+        text = petName.ToString() + " got " + coinNum.ToString() + " coins from getting a grade of " + grade.ToString() + " on assignment " + assignmentName + ".\n";
     }
 }
--- CanvasPetUnity/Assets/Pet.cs
+++ CanvasPetUnity/Assets/Pet.cs
@@ -1,4 +1,8 @@
 using System;
+using System.IO;
+using TMPro;
+using UnityEditor;
+using UnityEngine.Assertions;
 
 
 public class Pet
@@ -27,6 +31,17 @@ public class Pet
         name = "TEST_PET";
         currentFood = 0;
         storedFood = 0;
+        birthday = DateTime.Now;
+    }
+
+    public Pet(string nameString, DateTime grad)
+    {
+        currentHappiness = 0;
+        name = nameString;
+        currentFood = 0;
+        storedFood = 0;
+        birthday = DateTime.Now;
+        graduationDate = grad;
     }
 
     public int GetCurrentFood()
@@ -65,8 +80,98 @@ public class Pet
         birthday = birth;
     }
 
-    public void SetGraduation()
-    { }
+    public void SetGraduation(DateTime gradDate)
+    {
+        graduationDate = gradDate;
+    }
+
+    public void SaveBackup()
+    {
+        object[] objects = new object[8];
+        objects[0] = name;
+        objects[1] = MAX_FOOD;
+        objects[2] = HUNGER_THRESHOLD;
+        objects[3] = numCoins;
+        objects[4] = graduationDate;
+        objects[5] = birthday;
+        objects[6] = storedFood;
+        objects[7] = MAX_HAPPY;
+        string filepath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        StreamWriter sw = new StreamWriter(filepath + "/CanvasCritterBackup.txt");
+        
+
+        for(int i = 0; i< objects.Length; i ++)
+        {
+            sw.WriteLine(objects[i]);
+        }
+        sw.Close();
+
+        if (File.Exists(filepath) && CheckBackupFile(filepath))
+        {
+            Console.WriteLine("Successfully saved backup to " + filepath +"\n");
+        }
+        else
+        {
+            Console.WriteLine("Backup failed\n");
+        }
+    }
+
+    public bool CheckBackupFile(string filename)
+    {
+        string[] backupText = File.ReadAllLines(filename);
+        if (backupText.Length != 8)
+        {
+            return false;
+        }
+        if (!Int32.TryParse(backupText[1], out int t) || t != MAX_FOOD)
+        {
+            return false;
+        }
+        if (!Int32.TryParse(backupText[2], out int u) || u != HUNGER_THRESHOLD)
+        {
+            return false;
+        }
+        if (!Int32.TryParse(backupText[3], out int v))
+        {
+            return false;
+        }
+        if (!Int32.TryParse(backupText[7], out int w) || w != MAX_HAPPY)
+        {
+            return false;
+        }
+        if (!Int32.TryParse(backupText[6], out int x))
+        {
+            return false;
+        }
+        if (!DateTime.TryParse(backupText[4], out DateTime y))
+        {
+            return false;
+        }
+        if (!DateTime.TryParse(backupText[5], out DateTime z))
+        {
+            return false;
+        }
+        else return true;
+    }
+
+    public void BuildFromBackup(string filename)
+    {
+        if (CheckBackupFile(filename))
+        {
+            string[] backupText = File.ReadAllLines(filename);
+            numCoins = Int32.Parse(backupText[3]);
+            name = backupText[0];
+            graduationDate = DateTime.Parse(backupText[4]);
+            birthday = DateTime.Parse(backupText[5]);
+            storedFood = Int32.Parse(backupText[6]);
+            Console.WriteLine("Built " + name + " from backup file.\n");
+        }
+        else
+        {
+            Console.WriteLine("Failed to build from backup.\n");
+        }
+
+    }
 }
 
 public class Hat
