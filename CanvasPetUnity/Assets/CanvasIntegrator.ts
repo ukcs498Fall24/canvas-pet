4f5c8b16e01f8e83e62f00b04161eaad341466f7
jnnhyatt <jnnhyatt@gmail.com>
2024-11-07 16:19:52 -0500

    switching to typescript

---
+++ CanvasPetUnity/Assets/CanvasIntegrator.ts
@@ -0,0 +1,44 @@
+import { type Assignment } from "./types"
+
+export class CanvasIntegrator {
+  private uuid?: string
+  private saveFile?: string // temp
+  private initialized: boolean = false
+  private knownAssignments?: Assignment[]
+  private activeAssignments?: Assignment[]
+
+  // Start is called before the first frame update
+  public start(): void {
+    if (!this.initialized) {
+      this.onFirstRun()
+    } else {
+      this.pullUpdates()
+    }
+  }
+
+  public pullUpdates(): void {
+    // Implementation here
+  }
+
+  private onFirstRun(): void {
+    this.initialized = true
+  }
+
+  private assignmentChecker(newAssignments: Assignment[]): Assignment[] {
+    const updates: Assignment[] = newAssignments
+    for (const assignment of updates) {
+      // Implementation here
+    }
+    return updates
+  }
+
+  private validAssignment(assignment: Assignment): boolean {
+    let valid: boolean = true
+    if (assignment == null) {
+      valid = false
+    } else if (!assignment.published) {
+      valid = false
+    }
+    return valid
+  }
+}
