517aa36fa4a6f0b37a311037b7ead26a3f96f4f5
Hal1903 <hahikeyuaono0419@gmail.com>
2024-11-25 14:43:41 -0500

    background and gif

--- content.tsx
+++ content.tsx
@@ -7,7 +7,8 @@ import React, { useEffect, useState } from "react"
 import PetController from "./lib/PetController"
 import iconImage from "data-base64:~/assets/icon.png";
 import AnimImage from "data-base64:~/assets/saddog.gif";
-
+// https://www.freepik.com/free-vector/nature-roadside-background-scene_40169781.htm#fromView=keyword&page=1&position=29&uuid=aba69c23-0aa3-46c8-a02a-3c9e16651311
+import BGimage from "data-base64:~/assets/bg.jpg";
 import { getAssignmentGroups, getCourses } from "~lib/api"
 import type { Assignment, Course } from "~lib/types"
 
@@ -80,14 +81,42 @@ export default function Main() {
         style={{
           backgroundColor: "#ccc",
           borderRadius: "5px",
+          position: "relative", // Enable positioning for child elements
           width: "256px",
           height: "256px",
           display: "flex",
           alignItems: "center",
           justifyContent: "center"
         }}>
-        <p>Canvas Pet Goes Here!</p>
-        <img src={AnimImage} alt="Icon" style={{ width: "128px", height: "128px", marginTop: "5px" }} />
+        <img
+          src={BGimage}
+          alt="Background"
+          style={{
+            position: "absolute", // Make it fill the container
+            top: 0,
+            left: 0,
+            width: "100%", // covers the parent dim
+            height: "100%",
+            objectFit: "cover", // Prevent distortion
+            zIndex: 1 // Send it to the back
+          }}
+        />
+        <img src={AnimImage} alt="Icon" style={{
+          position: "relative", // Stacks on top of the background
+          zIndex: 2,
+          width: "75%",
+          height: "75%",
+          objectFit: "contain", // Maintain aspect ratio
+          }} 
+        />
+        <p style={{
+          position: "absolute", // Float above everything
+          top: "10px",
+          left: "50%", // Center horizontally
+          transform: "translateX(-50%)", // Fine-tune centering
+          zIndex: 3,
+        }}>Canvas Pet</p>
+        
       </div>
     </div>
   );
